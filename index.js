929c5331329434716a5dca4927f52b9b8542e8c2
Wes Bourke <wesbourkeavl@gmail.com>
2022-02-23 13:18:32 -0500

    commit tree b5fe504ff52d1f27491de1435df2124a6be7b7d2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+let saturdayFun = function (activity = 'roller-skate') {
+    return 'This Saturday, I want to ' + activity + '!';
+}
+
+let mondayWork = function (action = "go to the office") {
+    return "This Monday, I will " + action + ".";
+}
+
+let wrapAdjective = function (passThrough = "*") {
+    return function(parameter = "special"){
+        return `You are ${passThrough}${parameter}${passThrough}!`
+    }
+}
